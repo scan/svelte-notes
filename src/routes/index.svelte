0cf570724023ec33042f7ed52eaff53ce69dbca9
Tom Streller <tom@streller.me>
2022-02-12 19:35:24 +0100

    add button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-    import NoteList from "$lib/components/NoteList.svelte";
+    import TiDocumentAdd from 'svelte-icons/ti/TiDocumentAdd.svelte';
+
+    import NoteList from '$lib/components/NoteList.svelte';
 </script>
 
 <main class="m-2 min-h-[75vh] card card-bordered flex flex-row">
-    <div class="basis-1/4">
+    <div class="basis-1/4 p-4">
         <NoteList />
-        <div class="card card-bordered">
-            Add note
-        </div>
+        <button class="btn btn-outline btn-primary w-full flex flex-row justify-center">
+            <span class="w-8 h-8">
+                <TiDocumentAdd />
+            </span>
+        </button>
     </div>
     <div class="basis-3/4" />
 </main>
